9deadecbd678a5c3cbdf2a487e073a1c613ed08c
pablo-moraes <pablomoraes2017@gmail.com>
2020-06-25 23:59:16 -0300

    Update II

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-let birthday, today = new Date()
-birthday.setFullYear(2001, 05, 20);
-// today.setFullYear(2020, 05, 20);
-
-document.write(Math.round(today - birthday)/(1000 * 60 * 60 * 24));
+let birth = new Date();
+let today = new Date();
+birth.setFullYear(2001, 05,20);
